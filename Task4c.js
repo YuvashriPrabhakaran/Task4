c3ffc1240ca3762ded5fe75510846b85b5f8564a
Yuvashri Prabhakaran <yuvashriyuvashri19@gmail.com>
2026-06-30 14:56:34 +0530

    Add files via upload

---
+++ Task4c.js
@@ -0,0 +1,58 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
+
+
+// Contact Form Validation
+
+const form=document.getElementById("contactForm");
+
+const error=document.getElementById("error");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+let name=document.getElementById("name").value.trim();
+
+let email=document.getElementById("email").value.trim();
+
+let message=document.getElementById("message").value.trim();
+
+if(name=="" || email=="" || message==""){
+
+error.textContent="Please fill all fields.";
+
+return;
+
+}
+
+let emailPattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if(!email.match(emailPattern)){
+
+error.textContent="Enter a valid email.";
+
+return;
+
+}
+
+error.style.color="green";
+
+error.textContent="Message Sent Successfully!";
+
+form.reset();
+
+});
